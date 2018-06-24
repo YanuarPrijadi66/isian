a501942d225d9b643dbf72f793bee797708b2e8d
Yanuar Prijadi <YanuarPrijadi66@gmail.com>
2018-06-24 10:16:02 +0700

    Delete nav.php

--- utama/application/views/p_siswa/awal.php
+++
@@ -1,9 +0,0 @@
-	<?php
-		$level = $this->session->userdata('level');
-		$pilih = $this->input->get('pl');
-	?>
-
-		<div class="row" id="idDataUtama"></div>
-		<input type="hidden" id="lapel" name="lapel" value="<?php echo $level;?>">
-		<input type="hidden" id="pilih" name="pilih" value="<?php echo $pilih;?>">
-	
--- utama/application/views/pages/template/bawah.php
+++
@@ -1,8 +0,0 @@
-		<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-		<script src="<?php echo base_url();?>utama/assets/adminlte2/dist/js/pages/dashboard2.js"></script>
-		<!-- AdminLTE for demo purposes -->
-		<script src="<?php echo base_url();?>utama/assets/adminlte2/dist/js/demo.js"></script>
-
-    </body>
-</html>
-
